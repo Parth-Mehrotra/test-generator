2cb66d553c13efc03ec8e34f85f5d01231f89b2a
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-02-09 19:50:57 -0500

    initial count of if statements

--- test_generator/src/main/java/com/parthmehrotra/test_generator/App.java
+++ test_generator/src/main/java/com/parthmehrotra/test_generator/App.java
@@ -1,7 +1,43 @@
 package com.parthmehrotra.test_generator;
 
+import java.io.FileInputStream;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.IfStmt;
+
+/**
+ * Hello world!
+ *
+ */
 public class App {
-	public static void main( String[] args ) {
-		System.out.println( "Hello World!" );
+	public static void main(String[] args) throws Exception {
+		FileInputStream in = new FileInputStream("Location.java");
+
+		CompilationUnit cu;
+		cu = JavaParser.parse(in);
+		in.close();
+
+		new MethodVisitor().visit(cu, null);
+	}
+	/**
+	 * Simple visitor implementation for visiting MethodDeclaration nodes. 
+	 */
+	private static class MethodVisitor extends VoidVisitorAdapter {
+
+		@Override
+		public void visit(MethodDeclaration n, Object arg) {
+			n.accept(new IfVisitor(), null);
+			System.out.println("ran accept");
+		}
+	}
+
+	private static class IfVisitor extends VoidVisitorAdapter {
+		@Override
+		public void visit(IfStmt n, Object arg) {
+			System.out.println("Visited");
+			System.out.println(n);
+		}
 	}
 }
